d9bc322ec2321ae365305d8501e9e4b1b73c830a
Nishant Gaharwar <iamnishantgaharwar@gmail.com>
2023-03-14 21:09:36 +0530

    Add random colors in it.

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 addEventListener("click", createCircle);
 
+function getRandomColor(colorTag) {
+    const randomLetter = "123456789ABCDEF"
+    let hashTag = "#";
+    for (let i = 0; i < 6; i++) {
+        hashTag += randomLetter[Math.floor(Math.random() * 16)];
+    }
+    return hashTag;
+}
 function createCircle(e) {
     var circle = document.createElement("div");
     circle.className = "circle";
     circle.style.left = e.pageX + "px";
     circle.style.top = e.pageY  + "px";
+    circle.style.backgroundColor = getRandomColor();
     document.body.appendChild(circle);
 }
